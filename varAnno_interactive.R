76091a2f8135790db46089b1af83339a33a99038
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-11 11:26:40 -0400

    examine the vcf object

--- varAnno_interactive.R
+++ varAnno_interactive.R
@@ -8,5 +8,22 @@ library(VariantAnnotation)
 # read in the vcf file to a ExpandedVCF object - one variant per line
 vcf <- readVcf("inst/extdata/Challenge_data_(1).vcf", "hg19") %>% expand()
 
+################################################################################
+## examine the parameters of this object ##
 
+# look at the object
+vcf
+
+# what coordinates are represented in this file
+rowRanges(vcf)
+
+# how many variants are represented here?
+length(rowRanges(vcf))
+
+# what info is in the vcf header?
+info(header(vcf)) %>% as.data.frame()
+
+# what info is in the genotype/FORMAT field?
+geno(header(vcf))
+################################################################################
 
