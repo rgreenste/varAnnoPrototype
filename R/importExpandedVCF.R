fbdf60e366baa8b6d95686a25c762726981d6b66
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 19:32:28 -0400

    update importExpandedVCF.R to check dependencies

--- R/importExpandedVCF.R
+++ R/importExpandedVCF.R
@@ -1,3 +1,10 @@
 importExpandedVCF <- function(vcfFile, genome = "hg19") {
-  VariantAnnotation::expand(VariantAnnotation::readVcf(vcfFile, genome))
+
+  # check dependencies
+  if(!require(dplyr))
+    stop("the 'dplyr' package must be be installed first")
+  if(!require(VariantAnnotation))
+    stop("the 'variantAnnotation' package must be be installed first")
+
+  VariantAnnotation::readVcf(vcfFile, genome) %>% VariantAnnotation::expand()
 }
