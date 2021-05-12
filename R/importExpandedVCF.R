84ee130fe8f45b4f8dc57366aacaf413c8ac0428
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 19:43:32 -0400

    update importExpandedVCF.R to include checks for vcf file format

--- R/importExpandedVCF.R
+++ R/importExpandedVCF.R
@@ -6,5 +6,16 @@ importExpandedVCF <- function(vcfFile, genome = "hg19") {
   if(!require(VariantAnnotation))
     stop("the 'variantAnnotation' package must be be installed first")
 
-  VariantAnnotation::readVcf(vcfFile, genome) %>% VariantAnnotation::expand()
+  # check file format of vcf
+  if(!is.character(vcfFile))
+    stop("'vcfFile' should be character")
+  if(!(stringr::str_sub(vcfFile, start = -3, end = -1) %>% stringr::str_to_lower()) == "vcf")
+    stop("'vcfFile' must be a vcf file")
+
+  # check file exists then read in the vcf file and expand
+  if(file.exists(vcfFile)) {
+    VariantAnnotation::readVcf(vcfFile, genome) %>% VariantAnnotation::expand()
+  } else {
+    print("the file does not exist")
+  }
 }
