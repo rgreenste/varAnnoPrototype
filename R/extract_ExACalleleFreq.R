55556137ab357ccc6c89c3f1ea705d6855ca55c9
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-13 09:42:17 -0400

    add checks for genome and input file class

--- R/extract_ExACalleleFreq.R
+++ R/extract_ExACalleleFreq.R
@@ -1,5 +1,12 @@
 extract_ExACalleleFreq <- function(rowData){
 
+  # check the class of input file
+  if(!class(rowData) == "GRanges" )
+    stop("'rowData' should be of class 'GRanges'. Please read in a vcf file with the importExpandedVCF function and extract the rowData with 'extract_rowData()' before proceeding")
+
+  # check for correct genome
+  if(!GenomeInfoDb::genome(rowData)[1] == "hg19")
+    stop("for this release only 'hg19' is currently supported")
 
   # using a POST call perform a bulk query of ExAC database using the variant names
   res<- httr::POST("http://exac.hms.harvard.edu/rest/bulk/variant/variant", body = jsonlite::toJSON(rowData$ExACname))
