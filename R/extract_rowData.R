360d14b8287490aa3678256f36b202c3380a71c3
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 20:54:26 -0400

    add check for variants on standard chromosomes

--- R/extract_rowData.R
+++ R/extract_rowData.R
@@ -1,9 +1,18 @@
 extract_rowData <- function(expandedVCFfile) {
+
   # check the class of input file
   if(!class(expandedVCFfile) == "ExpandedVCF" )
     stop("'expandedVCFfile' should be of class 'ExpandedVCF'. Please read in a vcf file with the importExpandedVCF function")
 
   # extract the rowRanges from the VCF file
   rd<-rowRanges(expandedVCFfile)
+
+  # check that only main chromosomes have variants
+  if(!all(names(which(table(GenomeInfoDb::seqnames(rd)) > 0)) %in% c(seq(1:22), "X", "Y")))
+    warning('vcf file contains variants on nonstandard chromsomes - these will not be considered in this analysis')
+
+  # keep only the standard chromosomes
+  rd<-GenomeInfoDb::keepSeqlevels(rd, c(seq(1:22), "X", "Y"))
+
   rd
 }
