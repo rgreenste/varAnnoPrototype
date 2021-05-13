38691972668eff825058a6635742850518dce75f
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 21:02:15 -0400

    add documentation for extract_rowData.R

--- R/extract_rowData.R
+++ R/extract_rowData.R
@@ -1,3 +1,13 @@
+#' Extract rowData from ExpandedVCF file
+#'
+#' Extract the rowData from ExpandedVCF as GRanges object and convert seqlevels/chromsome names to UCSC style
+#'
+#' @param expandedVCFfile object of ExpandedVCF class
+#'
+#' @return GRanges object containing genomic coordinates for each variant and in the metadata columns Reference (REF) and Alternate (ALT) alleles, as well as quality scores (QUAL) and filter status
+#' @export
+#'
+#' @examples \dontrun{extract_rowData(expandedVCFfile)}
 extract_rowData <- function(expandedVCFfile) {
 
   # check the class of input file
@@ -5,7 +15,7 @@ extract_rowData <- function(expandedVCFfile) {
     stop("'expandedVCFfile' should be of class 'ExpandedVCF'. Please read in a vcf file with the importExpandedVCF function")
 
   # extract the rowRanges from the VCF file
-  rd<-rowRanges(expandedVCFfile)
+  rd<-SummarizedExperiment::rowRanges(expandedVCFfile)
 
   # check that only main chromosomes have variants
   if(!all(names(which(table(GenomeInfoDb::seqnames(rd)) > 0)) %in% c(seq(1:22), "X", "Y")))
@@ -15,7 +25,7 @@ extract_rowData <- function(expandedVCFfile) {
   rd<-GenomeInfoDb::keepSeqlevels(rd, c(seq(1:22), "X", "Y"))
 
   # convert to UCSC styles
-  newStyle <- GenomeInfoDb::mapSeqlevels(seqlevels(rd), "UCSC")
+  newStyle <- GenomeInfoDb::mapSeqlevels(GenomeInfoDb::seqlevels(rd), "UCSC")
   rd <- GenomeInfoDb::renameSeqlevels(rd, newStyle)
 
   rd
