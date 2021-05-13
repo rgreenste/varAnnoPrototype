126c0a2980aaa92a1d91a9df069d467f18aeecac
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-13 09:25:26 -0400

    updated fuction to add metadata column containing variant name in ExAC style

--- R/extract_rowData.R
+++ R/extract_rowData.R
@@ -4,7 +4,7 @@
 #'
 #' @param expandedVCFfile object of ExpandedVCF class
 #'
-#' @return GRanges object containing genomic coordinates for each variant and in the metadata columns Reference (REF) and Alternate (ALT) alleles, as well as quality scores (QUAL) and filter status
+#' @return GRanges object containing genomic coordinates for each variant and in the metadata columns Reference (REF) and Alternate (ALT) alleles, as well as quality scores (QUAL) and filter status and variant name in ExAC style (chromosome-position-reference-alternate)
 #' @export
 #'
 #' @examples \dontrun{extract_rowData(expandedVCFfile)}
@@ -27,6 +27,9 @@ extract_rowData <- function(expandedVCFfile) {
   # convert to UCSC styles
   newStyle <- GenomeInfoDb::mapSeqlevels(GenomeInfoDb::seqlevels(rd), "UCSC")
   rd <- GenomeInfoDb::renameSeqlevels(rd, newStyle)
+
+  # make a new metadata column on the rowData in the ExAC variant naming style
+  GenomicRanges::mcols(rd)$ExACname <- paste(stringr::str_remove(GenomicRanges::seqnames(rd), "chr"), GenomicRanges::start(rd), rd$REF, rd$ALT, sep="-")
 
   rd
 }
