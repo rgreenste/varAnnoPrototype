6c8ac31c49a53013a2d35e8ef0f80f96149eea27
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 23:54:23 -0400

    update function to keep only one annotation per variant of the feature location of most severe biological consequence if mutated (1) reorder the levels of the location factor by biological severity for mutations within that type of region (2) collapse the annotation GRanges object to keep only rows containing the location of most biological importance and keep only one transcript per gene (3) extract location and gene ID numbers for each annotation and return a tibble object, annotations are tracked to original file by their query ID

--- R/extract_variantLocation.R
+++ R/extract_variantLocation.R
@@ -1,3 +1,11 @@
+#' Annotate Variants Based on Genomic Feature Location
+#'
+#' @param rowData GRanges object containing genomic coordinates for each variant and Reference (REF) and Alternate (ALT) alleles
+#'
+#' @return tibble object containing information about the location of each variant with respect to genomic features and if located within a gene additionally the Gene ID number for that annotation
+#' @export
+#'
+#' @examples \dontrun{extract_variantLocation(rowData)}
 extract_variantLocation <- function (rowData) {
 
    # check for correct genome
@@ -8,5 +16,21 @@ extract_variantLocation <- function (rowData) {
   allvar <- VariantAnnotation::locateVariants(rd, # rowRanges from vcf renamed to have UCSC style chromosomes
                                               TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene, #UCSC txdb object
                                               AllVariants()) # what variants to annotate - all of them
-  allvar
+
+  # explicitly reorder the levels of the LOCATION factor by order of assumed 'severity' if mutated
+  allvar$LOCATION <- factor(allvar$LOCATION, levels = c("coding", "spliceSite", "fiveUTR", "promoter", "threeUTR", "intron", "intergenic"))
+
+  # group by QUERYID (the row number from the vcf file / rd (rowData))
+  # keep rows based on location type in order of worst "consequence"
+  # keep one entry per gene (based on smallest TXID) if found in multiple transcripts
+  allvar_collapsed <- allvar %>% data.frame() %>% dplyr::mutate(TXID_noNA = tidyr::replace_na(TXID, "*")) %>% # convert NAs in TXID to *
+    dplyr::group_by(QUERYID) %>% dplyr::slice_min(LOCATION) %>% # keep the smallest value (most "severe") of LOCATION factor per QUERYID
+    dplyr::slice_min(TXID_noNA) %>% # keep the smallest transcript based on TXID_noNA if variant found in more than on TXID
+    dplyr::select(LOCATION, QUERYID, GENEID) # keep columns of interest for simplicity
+
+  # make sure there is only one row per QUERYID
+  if(!table(allvar_collapsed$QUERYID) %>% max() == 1)
+    stop("There is more than one row per variant. Subsequent joins will fail. Additional redundancies must be evaluated")
+
+  allvar_collapsed
 }
