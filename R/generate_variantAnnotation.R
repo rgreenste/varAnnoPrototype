b54b1218df275ab1826125fb08f9b558fddc43c3
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-13 11:11:06 -0400

    add checks for variable class and row number consistency

--- R/generate_variantAnnotation.R
+++ R/generate_variantAnnotation.R
@@ -1,5 +1,17 @@
 generate_variantAnnotation <- function(rowData, infoMetrics, variantLocation, variantConsequence, exacAlleleFreq){
 
+  # check the class of input file
+  if(!class(rowData) == "GRanges" )
+    stop(" Error:'rowData' should be of class 'GRanges'. Please read in a vcf file with the importExpandedVCF function and extract the rowData with 'extract_rowData()' before proceeding")
+
+  # check the class of input file
+  if(!class(infoMetrics) == "data.frame" )
+    stop("Error: 'infoMetrics' should be of class 'data.frame'. Please generate 'infoMetrics' via the 'extract_infoMetrics()' function before proceeding.")
+
+  # check lengths match - should be as they come from same ExpandedVCF object with no subsetting of rows
+  if(!(nrow(infoMetrics) == length(rowData)))
+    stop("Error: 'infoMetrics' and 'rowData' are of different lengths. Check that both were generated from the same vcf file with no subsetting.")
+
 
   annotatedVCF_temp <- rowData %>% as.data.frame() %>% tibble::rowid_to_column("QUERYID") %>% # make a queryID column from rd row index
     dplyr::select(ExACname, chromosome = seqnames, start, end, REF, ALT, QUERYID) %>% # extract relevant columns from rd
