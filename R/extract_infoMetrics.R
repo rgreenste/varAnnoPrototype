4a5f20ef67cc7d967eed8219fd95d5b336e27c78
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 21:57:25 -0400

    add documentation for extract_infoMetrics.R

--- R/extract_infoMetrics.R
+++ R/extract_infoMetrics.R
@@ -1,3 +1,13 @@
+#' Extract Various Metrics from INFO Field of vcf File
+#'
+#' Extract metrics from the INFO field of a vcf file. Returns a data.frame containing one row for each variant and columns containing information about the type of variant, read depth, number of reads supporting the variant allele, number of reads supporting the reference allele, and the percentage of reads supporting the variant
+#'
+#' @param expandedVCFfile object of ExpandedVCF class
+#'
+#' @return dataframe object containing the requested information for the INFO field of the vcf file
+#' @export
+#'
+#' @examples \dontrun{extract_infoMetrics(expandedVCFfile)}
 extract_infoMetrics <- function(expandedVCFfile) {
 
   # check the class of input file
@@ -14,7 +24,7 @@ extract_infoMetrics <- function(expandedVCFfile) {
                   total_read_depth = DP, #depth of sequencing coverage at site of variation
                   variant_read_count = AO, #number of reads supporting the variant
                   reference_read_count = RO, #number of of reads supporting the reference
-                  percent_variant_reads = 100*AO/DP) %>% #percentage of reads supporting the variant
+                  percent_variant_reads = round(100*AO/DP,2)) %>% #percentage of reads supporting the variant
     dplyr::select(variant_type, total_read_depth, variant_read_count, reference_read_count, percent_variant_reads)
 
 }
