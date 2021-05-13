a646d450f7d899a2aa587479ed1664e4467bda07
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 22:31:00 -0400

    add a check for correct genome build
    at this time only hg19 is supported

--- R/extract_variantLocation.R
+++ R/extract_variantLocation.R
@@ -1,5 +1,9 @@
 extract_variantLocation <- function (rowData) {
 
+   # check for correct genome
+  if(!genome(rd)[1] == "hg19")
+    stop("for this release only 'hg19' is currently supported")
+
   # annotate variants based on txdb
   allvar <- VariantAnnotation::locateVariants(rd, # rowRanges from vcf renamed to have UCSC style chromosomes
                                               TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene, #UCSC txdb object
