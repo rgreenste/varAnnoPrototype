b7b755f3d9788563699d757d298c690217c02cff
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-13 18:25:32 -0400

    update error messages

--- R/importExpandedVCF.R
+++ R/importExpandedVCF.R
@@ -12,22 +12,22 @@ importExpandedVCF <- function(vcfFile, genome = "hg19") {
 
   # check file format of vcf
   if(!is.character(vcfFile))
-    stop("'vcfFile' should be character")
+    stop("Error: 'vcfFile' should be character")
   if(!(stringr::str_sub(vcfFile, start = -3, end = -1) %>% stringr::str_to_lower()) == "vcf")
-    stop("'vcfFile' must be a vcf file")
+    stop("Error:'vcfFile' must be a vcf file")
 
   # check name of genome, must be one of possible human genome assemblies
   if(!genome %in% c("hg18", "hg19", "hg38"))
-    stop("'genome' should be one of 'hg18', 'hg19', 'hg38'")
+    stop("Error: 'genome' should be one of 'hg18', 'hg19', 'hg38'")
 
   # for this release only hg19 is currently supported
   if(!genome == "hg19")
-    stop("for this release only 'hg19' is currently supported")
+    stop("Error: For this release only 'hg19' is currently supported")
 
   # check file exists then read in the vcf file and expand
   if(file.exists(vcfFile)) {
     VariantAnnotation::readVcf(vcfFile, genome) %>% VariantAnnotation::expand()
   } else {
-    print("the file does not exist")
+    print("Error: The file does not exist. Please use a valid file.")
   }
 }
