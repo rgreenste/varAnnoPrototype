cc71731f1bf68110ce75a95501623b44fb99abd8
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-13 18:31:48 -0400

    update error messages

--- R/extract_rowData.R
+++ R/extract_rowData.R
@@ -12,14 +12,14 @@ extract_rowData <- function(expandedVCFfile) {
 
   # check the class of input file
   if(!class(expandedVCFfile) == "ExpandedVCF" )
-    stop("'expandedVCFfile' should be of class 'ExpandedVCF'. Please read in a vcf file with the importExpandedVCF function")
+    stop("Error: 'expandedVCFfile' should be of class 'ExpandedVCF'. Please read in a vcf file with the importExpandedVCF function")
 
   # extract the rowRanges from the VCF file
   rd<-SummarizedExperiment::rowRanges(expandedVCFfile)
 
   # check that only main chromosomes have variants
   if(!all(names(which(table(GenomeInfoDb::seqnames(rd)) > 0)) %in% c(seq(1:22), "X", "Y")))
-    warning('vcf file contains variants on nonstandard chromsomes - these will not be considered in this analysis')
+    warning('Warning: vcf file contains variants on nonstandard chromsomes - these will not be considered in this analysis')
 
   # keep only the standard chromosomes
   rd<-GenomeInfoDb::keepSeqlevels(rd, c(seq(1:22), "X", "Y"))
