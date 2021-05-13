e6dda5854a0133878a42b47d8c05c6f1cb8903ae
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-13 00:19:23 -0400

    add checks for input file class and genome build

--- R/extract_variantConsequence.R
+++ R/extract_variantConsequence.R
@@ -1,5 +1,17 @@
 extract_variantConsequence <- function (rowData, expandedVCFfile){
 
+  # check the class of input file
+  if(!class(rowData) == "GRanges" )
+    stop("'rowData' should be of class 'GRanges'. Please read in a vcf file with the importExpandedVCF function and extract the rowData with 'extract_rowData()' before proceeding")
+
+  # check for correct genome
+  if(!GenomeInfoDb::genome(rowData)[1] == "hg19")
+    stop("for this release only 'hg19' is currently supported")
+
+  # check the class of input file
+  if(!class(expandedVCFfile) == "ExpandedVCF" )
+    stop("'expandedVCFfile' should be of class 'ExpandedVCF'. Please read in a vcf file with the importExpandedVCF function")
+
   # predict coding variants using rowData, txdb, Hspaiens BSGenome object, and alt accessor of vcf
   coding <- VariantAnnotation::predictCoding(rowData, # rowRanges from vcf renamed to have UCSC style chromosomes
                                              TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene, #UCSC txdb object containing feature annotations
