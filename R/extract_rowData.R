fe46440ecd5e98da098cdcd705cbe1f88842eb2e
rgreenste <26193137+rgreenste@users.noreply.github.com>
2021-05-12 20:37:12 -0400

    check expandedVCFfile is of class ExpandedVCF

--- R/extract_rowData.R
+++ R/extract_rowData.R
@@ -1,4 +1,8 @@
 extract_rowData <- function(expandedVCFfile) {
+  # check the class of input file
+  if(!class(expandedVCFfile) == "ExpandedVCF" )
+    stop("'expandedVCFfile' should be of class 'ExpandedVCF'. Please read in a vcf file with the importExpandedVCF function")
+
   # extract the rowRanges from the VCF file
   rd<-rowRanges(expandedVCFfile)
   rd
